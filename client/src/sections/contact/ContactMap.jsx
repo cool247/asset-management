2ba4cb59fe3f86ebb980cde63a2d8c46de601eaf
Sadanand Gopal <sadanandgpl@gmail.com>
2024-11-18 21:48:08 +0530

    updated

--- client/src/sections/contact/ContactMap.jsx
+++ client/src/sections/contact/ContactMap.jsx
@@ -42,7 +42,51 @@ export default function ContactMap() {
 
   return (
     <RootStyle>
-      
+      {/* <MapGL
+        {...viewport}
+        onViewportChange={setViewport}
+        mapStyle={`mapbox://styles/mapbox/${isLight ? 'light' : 'dark'}-v10`}
+        mapboxApiAccessToken={MAPBOX_API}
+        width="100%"
+        height="100%"
+      >
+        <MapControlScale />
+        <MapControlNavigation />
+
+        {_mapContact.map((country) => (
+          <MapControlMarker
+            key={country.phoneNumber}
+            latitude={country.latlng[0]}
+            longitude={country.latlng[1]}
+            onClick={() => setTooltip(country)}
+          />
+        ))}
+
+        {tooltip && (
+          <MapControlPopup
+            longitude={tooltip.latlng[1]}
+            latitude={tooltip.latlng[0]}
+            onClose={() => setTooltip(null)}
+            sx={{
+              '& .mapboxgl-popup-content': { bgcolor: 'common.white' },
+              '&.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip': { borderTopColor: '#FFF' },
+              '&.mapboxgl-popup-anchor-top .mapboxgl-popup-tip': { borderBottomColor: '#FFF' },
+            }}
+          >
+            <Typography variant="subtitle2" sx={{ mb: 0.5 }}>
+              Address
+            </Typography>
+            <Typography component="p" variant="caption">
+              {tooltip.address}
+            </Typography>
+
+            <Typography component="p" variant="caption" sx={{ mt: 1, display: 'flex', alignItems: 'center' }}>
+              <Iconify icon={'eva:phone-fill'} sx={{ mr: 0.5, width: 14, height: 14 }} />
+              {tooltip.phoneNumber}
+            </Typography>
+          </MapControlPopup>
+        )}
+      </MapGL> */}
     </RootStyle>
   );
 }
