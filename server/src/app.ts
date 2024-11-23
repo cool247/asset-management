32360565d3d4c8442646122c358f50a2274e7842
Sadanand Gopal <sadanandgpl@gmail.com>
2024-11-23 23:54:54 +0530

    add rate-limiter

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-
+import rateLimit from "@fastify/rate-limit";
 // import { db } from "./Config/db";
 // import { migrate } from "drizzle-orm/node-postgres/migrator";
 
@@ -9,6 +9,20 @@ import { config } from "./Config/config";
 
 const main = async () => {
   const app = await buildServer();
+
+  await app.register(rateLimit, {
+    max: 4, // Maximum 4 requests
+    timeWindow: "1 second", // Per 1 second
+    keyGenerator: (request) => request.ip, // Use the IP address for rate limiting
+    errorResponseBuilder: (req, context) => {
+      return {
+        statusCode: 429,
+        error: "Too Many Requests",
+        message: `Rate limit exceeded. Try again in ${Math.ceil(context.ttl / 1000)} seconds.`,
+      };
+    },
+  });
+
 
   app.listen(
     {
