6e3e604c638c16da5ffa71064e297ff0ad66bcee
Sadanand Gopal <sadanandgpl@gmail.com>
2024-11-23 14:42:09 +0530

    validation correction

--- server/src/Routes/user.route.ts
+++ server/src/Routes/user.route.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { createUser, getAllUsers, getUserById, updateUserById, deleteUserById } from "../Controllers/user.controller";
-import { createUserSchema, updateUserSchema, userIdSchema } from "../Schemas/user.schema";
+import { createUserSchema, updateUserSchema } from "../Schemas/user.schema";
 import { validate } from "../Middleware/validation.middleware";
 import { JwtPayload } from "../Utils/types";
 
@@ -41,7 +41,7 @@ const authorize = (roles: string[]) => {
 export const userRoutes = async (app: FastifyInstance) => {
   app.post("/create-new", { preHandler: [validate(createUserSchema)] }, createUser);
   app.get("/", getAllUsers);
-  app.get("/:id", { preHandler: [validate(userIdSchema)] }, getUserById);
+  app.get("/:id", getUserById);
   app.patch("/:id", { preHandler: [validate(updateUserSchema)] }, updateUserById);
-  app.delete("/:id", { preHandler: [validate(userIdSchema)] }, deleteUserById);
+  app.delete("/:id", deleteUserById);
 };
