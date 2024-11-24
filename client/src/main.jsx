7f37d0fb485d554d6653545b0f2c2e741396dbde
Prashant Singh <prashant.singh@dbuggedprogrammers.com>
2024-11-24 23:49:13 +0530

    added font

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,52 +1,53 @@
 // i18n
-import './locales/i18n';
+import "./locales/i18n";
 
 // highlight
-import './utils/highlight';
+import "./utils/highlight";
 
 // scroll bar
-import 'simplebar/src/simplebar.css';
+import "simplebar/src/simplebar.css";
 
 // lightbox
 
 // map
 
 // editor
-import 'react-quill/dist/quill.snow.css';
+import "react-quill/dist/quill.snow.css";
 
 // slick-carousel
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 // lazy image
-import 'react-lazy-load-image-component/src/effects/blur.css';
-import 'react-lazy-load-image-component/src/effects/opacity.css';
-import 'react-lazy-load-image-component/src/effects/black-and-white.css';
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "react-lazy-load-image-component/src/effects/opacity.css";
+import "react-lazy-load-image-component/src/effects/black-and-white.css";
 
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
 // @mui
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 // redux
-import { store, persistor } from './redux/store';
+import { store, persistor } from "./redux/store";
 // contexts
-import { SettingsProvider } from './contexts/SettingsContext';
-import { CollapseDrawerProvider } from './contexts/CollapseDrawerContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SettingsProvider } from "./contexts/SettingsContext";
+import { CollapseDrawerProvider } from "./contexts/CollapseDrawerContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // Check our docs
 
-import { AuthProvider } from './contexts/JWTContext';
+import { AuthProvider } from "./contexts/JWTContext";
 // import { AuthProvider } from './contexts/Auth0Context';
 // import { AuthProvider } from './contexts/FirebaseContext';
 // import { AuthProvider } from './contexts/AwsCognitoContext';
 // Create a client
 //
-import App from './App';
-const container = document.getElementById('root');
+import App from "./App";
+import "./index.css";
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 const queryClient = new QueryClient();
@@ -58,15 +59,15 @@ root.render(
     <ReduxProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <SettingsProvider>
-          <CollapseDrawerProvider>
-            <BrowserRouter>
-              <QueryClientProvider client={queryClient}>
-                <App />
-              </QueryClientProvider>
-            </BrowserRouter>
-          </CollapseDrawerProvider>
-        </SettingsProvider>
+          <SettingsProvider>
+            <CollapseDrawerProvider>
+              <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
+                  <App />
+                </QueryClientProvider>
+              </BrowserRouter>
+            </CollapseDrawerProvider>
+          </SettingsProvider>
         </LocalizationProvider>
       </PersistGate>
     </ReduxProvider>
