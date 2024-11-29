c19e1509dc2dfa3a6485790ba02ba05830691a25
Sadanand___ <minteckui@gmail.com>
2024-11-29 16:42:22 +0530

    add asset.controller

--- server/src/Schemas/asset.schema.ts
+++ server/src/Schemas/asset.schema.ts
@@ -1,10 +1,19 @@
 import { z } from "zod";
 import { createAssetItemSchema, updateAssetItemSchema } from "./asset-item.schema";
 
+const createPropertiesSchema = z.object({
+  propertyId:z.number().positive("Property ID must be positive"),
+  propertyValue:z.any().refine(
+    (val) => typeof val === "string" || typeof val === "number" || typeof val === "boolean",
+    { message: "Value must be a string, number, or boolean" }
+  ),
+})
+
 export const createAssetSchema = z.object({
   name: z.string().max(100, "name must be less than 100 characters"),
-  typeId: z.number().int("Asset Type ID must be an integer").nonnegative("Asset Type ID must be positive"),
+  typeId: z.number().int("Asset Type ID must be an integer").positive("Asset Type ID must be positive"),
   items: z.array(createAssetItemSchema),
+  propertiesAndValues: z.array(createPropertiesSchema),
 });
 
 export const updateAssetSchema = z.object({
